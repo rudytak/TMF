2a53fa9ea5cd0324207b61b9485d02c17d0f040f
rudytak <ondra4sedlacek@gmail.com>
2023-12-17 23:57:57 +0100

    .

--- 23-24/10/_SOČ/main.lol
+++ 23-24/10/_SOČ/main.lol
@@ -1,2 +1,2 @@
-\contentsline {lstlisting}{./prilohy/snippets/c1.py}{11}{lstlisting.-1}%
-\contentsline {lstlisting}{\numberline {1}{\ignorespaces Kód k analyzování spinenrového kyvadla}}{12}{lstlisting.1}%
+\contentsline {lstlisting}{./prilohy/snippets/c1.py}{12}{lstlisting.-1}%
+\contentsline {lstlisting}{\numberline {1}{\ignorespaces Kód k analyzování spinnerového kyvadla}}{12}{lstlisting.1}%
--- 23-24/10/_SOČ/prilohy/snippets/c1.py
+++ 23-24/10/_SOČ/prilohy/snippets/c1.py
@@ -1,46 +1,48 @@
+# imports
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 
+# helper function to convert the map iterator to a list
 lmap = lambda f,x : list(map(f, x))
-
-peaks_s = 0
-peaks_e = 0
+# peaks start and end points
+peaks_s, peaks_e = 0
 
 with open("./kyvadlo.csv", "r") as read:
-    # DATA
-    data = read.readlines()[1:]
+    data = read.readlines()[1:] # load all the value rows
+    # convert all rows into value arrays
     data = lmap(lambda row: row.rstrip().split(","), data)
 
-    xdata = lmap(lambda row: float(row[0]), data)
-    ydata = lmap(lambda row: float(row[1]), data)
-    mov = 15
+    xdata = lmap(lambda row: float(row[0]), data) # x axis data
+    ydata = lmap(lambda row: float(row[1]), data) # y axis data
+    mov = 15 # the amount of smoothing
+    # smooths out the data using convolution
     ydata_clean = np.convolve(ydata, np.ones(mov)/mov, "same")
 
-    # plt.plot(xdata, ydata)
-    samples_per_sec = (len(xdata) / max(xdata))
-
+    # find the peaks from the data using scipy.signal.find_peaks
     peaks, heights = sig.find_peaks(
         ydata_clean,
         height=1,
         threshold=0.0001,
         distance=35
     )
+    # get the sampling frequency
+    samples_per_sec = (len(xdata) / max(xdata))
+    # find the start and end time of where the peaks are
     peaks_s = peaks[0] - int(samples_per_sec / 2)
     peaks_e = peaks[-1] + int(samples_per_sec / 2)
 
-    plt.plot(
-        xdata[peaks_s: peaks_e], 
-        ydata_clean[peaks_s: peaks_e]
-    )
-
-    f = 1/2 * ((len(peaks)-1) / ((max(peaks) - min(peaks)) / samples_per_sec))
-    print(f"{f:.2f} Hz") 
-    print(max(peaks)/200 - min(peaks)/200, len(peaks)) 
-
+    # plot the smoothed out data
+    plt.plot(xdata[peaks_s: peaks_e], ydata_clean[peaks_s: peaks_e])
+    # plot all the peak lines
     for p in peaks:
         plt.axvline(p / samples_per_sec, color="r", linestyle=":")
 
-plt.title("Magnetic field from a spinner pendulum")
-plt.ylabel("$B$ $[mT]$")
-plt.xlabel("$t$ $[s]$")
+    # set all other styling of the plot
+    plt.title("Magnetic field from a spinner pendulum")
+    plt.ylabel("$B$ $[mT]$")
+    plt.xlabel("$t$ $[s]$")
+
+    # calcualte and print out the resulting frequency
+    f = 1/2 * ((len(peaks)-1) / ((max(peaks) - min(peaks)) / samples_per_sec))
+    print(f"{f:.2f} Hz") 
